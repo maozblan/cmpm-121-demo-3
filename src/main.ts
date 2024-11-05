3006d0ae69c54f942ae4426ef25eca9bacc0b33a
Lyssa Li <lyssa.li137@gmail.com>
2024-11-04 20:58:40 -0800

    adding starting button :D

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const button: HTMLButtonElement = document.createElement("button");
+button.textContent = "boopadooop";
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
+app.append(button);
