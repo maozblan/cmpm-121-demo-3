724ab8425488b6928b63032c16916cddfa8640e3
Lyssa Li <lyssa.li137@gmail.com>
2024-11-17 10:29:58 -0800

    global coordinate system anchored at null island

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,21 @@ import "./leafletWorkaround.ts";
 // Deterministic random number generator
 import luck from "./luck.ts";
 
-// Location of our classroom (as identified on Google Maps)
-const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+interface Cell {
+  i: number;
+  j: number;
+}
+
+function cellToLatLng(cell: Cell): leaflet.LatLng {
+  return leaflet.latLng(
+    cell.i * TILE_DEGREES,
+    cell.j * TILE_DEGREES,
+  );
+}
+
+// global coordinate system as defined by d1.b
+const OAKES_CLASSROOM: Cell = { i: 369894, j: -1220627 };
+const NULL_ISLAND: Cell = { i: 0, j: 0 };
 
 // Tunable gameplay parameters
 const GAMEPLAY_ZOOM_LEVEL = 19;
@@ -22,7 +35,7 @@ const CACHE_SPAWN_PROBABILITY = 0.1;
 
 // Create the map (element with id "map" is defined in index.html)
 const map = leaflet.map(document.getElementById("map")!, {
-  center: OAKES_CLASSROOM,
+  center: cellToLatLng(OAKES_CLASSROOM),
   zoom: GAMEPLAY_ZOOM_LEVEL,
   minZoom: GAMEPLAY_ZOOM_LEVEL,
   maxZoom: GAMEPLAY_ZOOM_LEVEL,
@@ -40,7 +53,7 @@ leaflet
   .addTo(map);
 
 // Add a marker to represent the player
-const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+const playerMarker = leaflet.marker(cellToLatLng(OAKES_CLASSROOM));
 playerMarker.bindTooltip("YOU");
 playerMarker.addTo(map);
 
@@ -59,7 +72,7 @@ updateStatusPanel();
 // Add caches to the map by cell numbers
 function spawnCache(i: number, j: number): void {
   // Convert cell numbers into lat/lng bounds
-  const origin = OAKES_CLASSROOM;
+  const origin = cellToLatLng(NULL_ISLAND);
   const bounds = leaflet.latLngBounds([
     [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
     [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
@@ -116,9 +129,10 @@ function spawnCache(i: number, j: number): void {
 // Look around the player's neighborhood for caches to spawn
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    // If location i,j is lucky enough, spawn a cache!
-    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      spawnCache(i, j);
+    const lat = i + OAKES_CLASSROOM.i;
+    const lng = j + OAKES_CLASSROOM.j;
+    if (luck([lat, lng].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(lat, lng);
     }
   }
 }
@@ -131,5 +145,3 @@ button.addEventListener("click", () => {
   alert("you clicked the button!");
 });
 app.append(button);
-
-// aha totally not super late :')
